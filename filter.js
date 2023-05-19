c710ae7aa73785fe9a8cf04e204d1ee774b275ad
fdy1993 <66513920+fdy1993@users.noreply.github.com>
2023-05-19 11:05:08 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,7 +1,9 @@
 // 过滤器函数，参数为节点对象，返回值为布尔值
-function filter(proxy) {
-  // 设置测试延迟的地址为https://www.google.com
+async function filter(proxy) {
+  // 设置测试延迟的地址
   proxy.testUrl = "http://cp.cloudflare.com/generate_204";
+  // 等待节点测试完成
+  await proxy.test();
   // 只保留延迟小于300ms的节点
   if (proxy.delay < 200) {
     return true;
