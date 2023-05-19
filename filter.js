3ae62b7a328912993764de8e46a894249e97a542
fdy1993 <66513920+fdy1993@users.noreply.github.com>
2023-05-19 13:11:43 +0800

    Update filter.js

--- filter.js
+++ filter.js
@@ -1,13 +1,16 @@
 // 过滤器函数，参数为节点对象，返回值为布尔值
 async function filter(proxy) {
   // 设置测试延迟的地址
-  proxy.testUrl = "http://cp.cloudflare.com/generate_204";
-  // 等待节点测试完成
-  await proxy.test();
-  // 只保留延迟小于300ms的节点
-  if (proxy.delay < 200) {
-    return true;
+  const proxy.testUrl = "http://cp.cloudflare.com/generate_204";
+  // 设置超时时间为3秒
+  const timeout = new Promise((resolve, reject) => setTimeout(() => reject("timeout"), 3000));
+  // 等待节点测试完成或超时
+  try {
+    await Promise.race([proxy.test(), timeout]);
+    // 只保留延迟小于200ms的节点
+    return proxy.delay < 200 ? true : false;
+  } catch (error) {
+    // 如果超时，返回false
+    return false;
   }
-  // 其他情况返回false
-  return false;
 }
