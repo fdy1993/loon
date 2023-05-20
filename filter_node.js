a1bb9345ae4ae0c0d6b4928fb84879d3e496c787
fandeyi <fmj19930621@gmial.com>
2023-05-20 09:53:07 +0800

    过滤地区节点

---
+++ filter_node.js
@@ -0,0 +1,9 @@
+// 这个函数用于过滤出名字含有香港、台湾、日本、新加坡或美国的节点
+// proxies: 一个代理对象的数组
+function filter(proxies) {
+    // 定义一个数组，包含要匹配的关键字
+    const keywords = ["香港", "台湾", "日本", "新加坡", "美国"];
+    // 使用map方法遍历代理数组，并返回一个布尔值，表示节点名是否含有任意一个关键字
+    return proxies.map(p => keywords.some(keyword => p.name.includes(keyword)));
+  }
+  
